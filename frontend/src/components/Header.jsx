05768f6aa6d0a1e491380d84b9641c88312e5d35
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-18 15:40:58 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import {Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 import {LinkContainer} from "react-router-bootstrap"
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  
+
+
   return (
     <header>
        <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -21,10 +26,18 @@ const Header = () => {
                     <LinkContainer to='/cart'>
                         <Nav.Link >
                           <FaShoppingCart /> Cart
+                          {
+                            cartItems.lenght > 0 && (
+                              <Badge pill bg='success' style={{
+                                marginLeft:'5px'}}>
+                                  {cartItems.reduce((a, c)=> a + c.qty,0)}
+                              </Badge>
+                            )
+                          }
                         </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to='/login'>
-                        <Nav.Link>
+                        <Nav.Link href='/login'>
                           <FaUser /> Sign In
                         </Nav.Link>
                     </LinkContainer>    

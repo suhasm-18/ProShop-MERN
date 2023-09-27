df33006639467d6dea3f38c239b4c99dcc39d39a
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-27 19:28:30 +0530

    Update userRouters.js

--- Banckend/routes/userRouters.js
+++ Banckend/routes/userRouters.js
@@ -10,3 +10,13 @@ import {
   getUserById,
   updateUser,
 } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.route("/").post(registerUser).get(getUsers);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;

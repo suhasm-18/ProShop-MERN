e1fb6532138232740f7e8acd9590c4e459e32267
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-27 19:28:03 +0530

    Create userRouters.js

---
+++ Banckend/routes/userRouters.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController.js";

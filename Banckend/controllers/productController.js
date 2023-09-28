f1525dd6ea1c4a1b314a98082868fc82470bf534
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-28 18:17:03 +0530

    Update productController.js

--- Banckend/controllers/productController.js
+++ Banckend/controllers/productController.js
@@ -15,3 +15,16 @@ const getProducts = asyncHandler(async (req, res) => {
 // @desc    Fetch all products
 // @route   GET /api/products/:id
 // @access  Public
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {getProducts , getProductById};

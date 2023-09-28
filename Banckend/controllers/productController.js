8556e0847d53e4834eeb521e30621d28502b4b37
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-28 18:16:29 +0530

    Create productController.js

---
+++ Banckend/controllers/productController.js
@@ -0,0 +1,17 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+
+
+// @desc    Fetch all products
+// @route   GET /api/products/:id
+// @access  Public

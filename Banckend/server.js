ea5a1f2a06a26134dbd3007c0e2bd3411dc6c3e8
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-28 18:15:00 +0530

    Update server.js

--- Banckend/server.js
+++ Banckend/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
-
+import userRoutes from './routes/userRouters.js';
 
 const port = process.env.PORT || 8000;
 
@@ -12,11 +12,16 @@ connectDB();
 
 const app = express();
 
+//Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send("API is runing");
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

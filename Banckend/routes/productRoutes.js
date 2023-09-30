888e92f61c22c4f658cc7196a310dc7e4362afae
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-30 18:43:08 +0530

    Update productRoutes.js

--- Banckend/routes/productRoutes.js
+++ Banckend/routes/productRoutes.js
@@ -1,25 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv'
-dotenv.config();
-import connectDB from './config/db.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRouters.js';
+import express from "express";
+const router = express.Router();
+import {getProducts , getProductById} from '../controllers/productController.js';
 
-const port = process.env.PORT || 8000;
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
-connectDB();
 
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("API is runing");
-});
-
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
-
-app.listen(port, () =>console.log(`Server running on port ${port}`));
+export default router;

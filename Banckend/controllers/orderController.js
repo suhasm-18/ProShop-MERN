cd4a9639716c762d381a7843c3552f17185ca9b2
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-03 09:48:25 +0530

    Update orderController.js

--- Banckend/controllers/orderController.js
+++ Banckend/controllers/orderController.js
@@ -4,7 +4,6 @@ import Order from "../models/orderModel.js";
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
-
 const addOrderItems = asyncHandler(async (req, res) => {
   const { orderItems, shippingAddress, paymentMethod,itemsPrice,
     taxPrice,

3b68d0386999d88b94d01f9e53b165698adb9a2e
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-04 09:54:28 +0530

    Update products.js

--- Banckend/data/products.js
+++ Banckend/data/products.js
@@ -59,4 +59,18 @@ const products = [
     rating: 3.5,
     numReviews: 10,
   },
- 
+  {
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/alexa.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;

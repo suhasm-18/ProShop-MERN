a11b2e10beb1905e3d8f7a96ed882c8577b47e71
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-07 17:47:57 +0530

    Update server.js

--- Banckend/server.js
+++ Banckend/server.js
@@ -1,10 +1,15 @@
+import path  from 'path';
 import express from 'express';
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRouters.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+
 
 const port = process.env.PORT || 8000;
 
@@ -16,14 +21,38 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//cookie cookieParser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send("API is runing");
 });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
-app.listen(port, () =>console.log(`Server running on port ${port}`));
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+

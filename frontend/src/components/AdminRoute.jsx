f718519cef6c82e325d5ded9d3c8c3edf24c0d41
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-09 19:26:48 +0530

    Create AdminRoute.jsx

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,6 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const AdminRoute = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  return userInfo && userInfo.isAdmin ? (

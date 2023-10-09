e3bd755903dd3d732145296432da1835c87b0fd9
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-09 19:27:04 +0530

    Update AdminRoute.jsx

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -4,3 +4,9 @@ import { useSelector } from 'react-redux';
 const AdminRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
   return userInfo && userInfo.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/login' replace />
+  );
+};
+export default AdminRoute;

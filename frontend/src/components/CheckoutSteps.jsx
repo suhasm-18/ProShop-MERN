1867be72c1de5e4428b8ccaddfd6cc79e3731a6c
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-09 19:28:06 +0530

    Update CheckoutSteps.jsx

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -24,4 +24,27 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
           <Nav.Link disabled>Shipping</Nav.Link>
         )}
       </Nav.Item>
+      <Nav.Item>
+        {step3 ? (
+          <LinkContainer to='/payment'>
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
 
+      <Nav.Item>
+        {step4 ? (
+          <LinkContainer to='/placeorder'>
+            <Nav.Link>Place Order</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Place Order</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default CheckoutSteps;

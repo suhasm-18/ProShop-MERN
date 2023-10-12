f926ac3f53b8241f2a9fd6a39e20a9482ffaa732
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-12 19:39:21 +0530

    Update Product.jsx

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -17,4 +17,17 @@ const Product = ({product}) => {
             </Card.Title>
         </Link>
 
+        <Card.Text as='div'>
+            <Rating value={product.rating} text={`${product.numReviews} 
+            Reviews`} />
+        </Card.Text>
 
+        <Card.Text as="h3">
+            ${product.price}
+        </Card.Text>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default Product

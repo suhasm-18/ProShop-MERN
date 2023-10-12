ce563686f3028667a2ca14e2b2414515df39befe
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-10-12 19:38:19 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import {Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom'
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 import {LinkContainer} from "react-router-bootstrap"
-import {useSelector} from 'react-redux'
+import {useSelector , useDispatch} from 'react-redux'
+import { useLogoutMutation } from '../slices/userApiSlice';
+import { logout } from '../slices/authSlice';
+import SearchBox from './SearchBox';
+import { resetCart } from '../slices/cartSlice';
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
-  
+  const { userInfo } = useSelector((state) => state.auth);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      dispatch(resetCart());
+      // NOTE: here we need to reset cart state for when a user logs out so the next
+      // user doesn't inherit the previous users cart and shipping
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <header>
@@ -23,30 +46,51 @@ const Header = () => {
                 <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
+                      <SearchBox/>
                     <LinkContainer to='/cart'>
                         <Nav.Link >
                           <FaShoppingCart /> Cart
-                          {
-                            cartItems.lenght > 0 && (
-                              <Badge pill bg='success' style={{
-                                marginLeft:'5px'}}>
-                                  {cartItems.reduce((a, c)=> a + c.qty,0)}
-                              </Badge>
-                            )
-                          }
+                          {cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                         </Nav.Link>
                     </LinkContainer>
-                    <LinkContainer to='/login'>
+                    {
+                      userInfo ? (
+                        <>
+                        <NavDropdown title={userInfo.name} id='username'>
+                          <LinkContainer to='/profile'>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                          </LinkContainer>
+                          <NavDropdown.Item onClick={logoutHandler}>
+                            Logout
+                          </NavDropdown.Item>
+                        </NavDropdown>
+                      </>
+                      ) : (
+                        <LinkContainer to='/login'>
                         <Nav.Link href='/login'>
                           <FaUser /> Sign In
                         </Nav.Link>
-                    </LinkContainer>    
+                    </LinkContainer>  
+                      )
+                    }  
+                    {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title="Admin" id='adminmenu'>
+                        <LinkContainer to='/admin/productlist'>
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/admin/orderlist'>
+                          <NavDropdown.Item>Orders</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/admin/userlist'>
+                        <NavDropdown.Item>Users</NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                    )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-       </Navbar>
-    </header>
-  )
-}
 
-export default Header
